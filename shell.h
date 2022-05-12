64029391f0b18a21ae347a9cd1526dbf717d4d4e
EWanjau <wamwanjau@gmail.com>
2022-05-12 09:42:55 +0000

    added scanner part prototypes to header file

--- shell.h
+++ shell.h
@@ -6,8 +6,31 @@
 #include "string.h"
 #include "errno.h"
 #include "unistd.h"
+
+/*for the scanner part*/
+#define EOF (-1)
+#define ERRCHAR (0)
+#define INIT_SRC_POS (-2)
+
+/*command prompt loop at the beginning*/
 void print_type1(void);
 void print_type2(void);
+
+/*read the input by the user*/
 char *read_command(void);
+
+/*get the input to the scanner*/
+struct source_s
+{
+	char *buffer;		/*the input commands by reader*/
+	long buffersize;	/*size of the text input*/
+	long charpos;		/*the char position from source text*/
+};
+
+/*functions to help read characters one at a time*/
+char next_char(struct source_s *src);
+void unget_char(struct source_s *src);
+char peek_char(struct source_s *src);
+void skip_white_spaces(struct source_s *src);
 
 #endif
