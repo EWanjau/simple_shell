06af7544a15443a24c3d11c225441a041af6d04f
EWanjau <wamwanjau@gmail.com>
2022-05-12 11:25:23 +0000

    header files

--- shell.h
+++ shell.h
@@ -13,13 +13,16 @@
 #define INIT_SRC_POS (-2)
 
 /*command prompt loop at the beginning*/
+
 void print_type1(void);
 void print_type2(void);
 
 /*read the input by the user*/
+
 char *read_command(void);
 
 /*get the input to the scanner*/
+
 struct source_s
 {
 	char *buffer;		/*the input commands by reader*/
@@ -28,9 +31,28 @@ struct source_s
 };
 
 /*functions to help read characters one at a time*/
+
 char next_char(struct source_s *src);
 void unget_char(struct source_s *src);
 char peek_char(struct source_s *src);
 void skip_white_spaces(struct source_s *src);
+
+/*parsing the input strings in small tokens*/
+
+struct token_s
+{
+	struct source_s *src;		/*input source (commands from cli)*/
+	int text_len;			/*length of token*/
+	char *text;			/*the token text*/
+};
+
+/*indicate the end of input*/
+
+extern struct token_s eof_token;
+
+/*captures and handles the tokens*/
+
+struct token_s *tokenize(struct source_s *src);
+void free_token(struct token_s *tok);
 
 #endif
